b7e15110784cea50f56c8d97df34ea4825723c8e
ababey <git@ababey.com>
2022-10-10 18:06:35 +0200

    Improve Phylogénie display

--- src/components/ClassificationTable/index.tsx
+++ src/components/ClassificationTable/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
 
 type Props = {
   plant: Queries.Plant;
 };
+
+// TODO: Factorize rows !
 
 const ClassificationTable = ({
   plant: {
@@ -24,60 +28,85 @@ const ClassificationTable = ({
   },
 }: Props) => {
   return (
-    <TableContainer>
-      <Table>
-        <TableBody>
-          {kingdom ? (
-            <TableRow>
-              <TableCell>Règne</TableCell>
-              <TableCell>{kingdom}</TableCell>
-            </TableRow>
-          ) : null}
-          {subKingdom ? (
-            <TableRow>
-              <TableCell>Sous-règne</TableCell>
-              <TableCell>{subKingdom}</TableCell>
-            </TableRow>
-          ) : null}
-          {division ? (
-            <TableRow>
-              <TableCell>Division</TableCell>
-              <TableCell>{division}</TableCell>
-            </TableRow>
-          ) : null}
-          {classP ? (
-            <TableRow>
-              <TableCell>Classe</TableCell>
-              <TableCell>{classP}</TableCell>
-            </TableRow>
-          ) : null}
-          {subClassP ? (
-            <TableRow>
-              <TableCell>Sous-classe</TableCell>
-              <TableCell>{subClassP}</TableCell>
-            </TableRow>
-          ) : null}
-          {order ? (
-            <TableRow>
-              <TableCell>Ordre</TableCell>
-              <TableCell>{order}</TableCell>
-            </TableRow>
-          ) : null}
-          {family ? (
-            <TableRow>
-              <TableCell>Famille</TableCell>
-              <TableCell>{family}</TableCell>
-            </TableRow>
-          ) : null}
-          {genus ? (
-            <TableRow>
-              <TableCell>Genre</TableCell>
-              <TableCell>{genus}</TableCell>
-            </TableRow>
-          ) : null}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box sx={{ flex: 1 }}>
+      <Typography
+        variant="h6"
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <AccountTreeIcon sx={{ marginX: 1 }} />
+        Phylogénie
+      </Typography>
+      <TableContainer>
+        <Table>
+          <TableBody>
+            {kingdom ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Règne</TableCell>
+                <TableCell>{kingdom}</TableCell>
+              </TableRow>
+            ) : null}
+            {subKingdom ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Sous-règne</TableCell>
+                <TableCell>{subKingdom}</TableCell>
+              </TableRow>
+            ) : null}
+            {division ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Division</TableCell>
+                <TableCell>{division}</TableCell>
+              </TableRow>
+            ) : null}
+            {classP ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Classe</TableCell>
+                <TableCell>{classP}</TableCell>
+              </TableRow>
+            ) : null}
+            {subClassP ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Sous-classe</TableCell>
+                <TableCell>{subClassP}</TableCell>
+              </TableRow>
+            ) : null}
+            {order ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Ordre</TableCell>
+                <TableCell>{order}</TableCell>
+              </TableRow>
+            ) : null}
+            {family ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Famille</TableCell>
+                <TableCell>{family}</TableCell>
+              </TableRow>
+            ) : null}
+            {genus ? (
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>Genre</TableCell>
+                <TableCell>{genus}</TableCell>
+              </TableRow>
+            ) : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 
